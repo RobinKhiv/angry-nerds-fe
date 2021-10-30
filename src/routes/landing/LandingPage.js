fe1a5826e8696d98849b486421b2c885de5a102d
Uyen-nt-Le <uyenle8897@csu.fullerton.edu>
2021-10-30 16:37:09 -0700

    landing page

--- src/routes/landing/LandingPage.js
+++ src/routes/landing/LandingPage.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './LandingPage.css'
+import React from "react";
+import "./LandingPage.css";
 
 export default function LandingPage() {
-    return (
-        <div>
-            landing page
-        </div>
-    )
+  return (
+    <div class="landing">
+      <h1>Welcome Angry Nerds!</h1>
+      <h2>Become an angry nerd today.</h2>
+      <div class="btn">
+        <button type="button">Sign up</button>
+      </div>
+      <h2>Already an angry nerd? Log in.</h2>
+      <div class="btn">
+        <button type="button">Log in</button>
+      </div>
+    </div>
+  );
 }
