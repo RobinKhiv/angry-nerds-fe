b83738eb25f2bb3d1e3d8a5a3ba048ff13ef8818
Robin <robinkhiv@gmail.com>
2021-10-30 11:57:30 -0700

    updated with test data

--- src/routes/content/ContentPage.js
+++ src/routes/content/ContentPage.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-export default function ContentPage() {
+export default function ContentPage(props) {
+    const data = {
+        roomId: '12345',
+        title: 'forum1',
+        author: 'angryperson1',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper tincidunt nulla, ut hendrerit ex auctor eget. Praesent a molestie nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel nisl id orci bibendum convallis at a purus. Quisque ut tortor sagittis, varius felis quis, imperdiet elit.',
+        comments: [{user:'angryperson2', content:'Lorem ipsum dolor sit amet'},{user:'angryperson2', content:'Lorem ipsum dolor sit amet'},{user:'angryperson2', content:'Lorem ipsum dolor sit amet'}]
+    }
     return (
         <div>
             content
--- src/routes/dashboard/DashboardPage.js
+++ src/routes/dashboard/DashboardPage.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
+
 export default function DashboardPage() {
+    const data = {
+        content: [ {title: 'forum1',user: 'angry1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper tincidunt nulla, ut hendrerit ex auctor eget. Praesent a molestie nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel nisl id orci bibendum convallis at a purus. Quisque ut tortor sagittis, varius felis quis, imperdiet elit.'},
+        {title: 'forum1',user: 'angryperson1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper tincidunt nulla, ut hendrerit ex auctor eget. Praesent a molestie nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel nisl id orci bibendum convallis at a purus. Quisque ut tortor sagittis, varius felis quis, imperdiet elit.'},
+        {title: 'forum2',user: 'angryperson2', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper tincidunt nulla, ut hendrerit ex auctor eget. Praesent a molestie nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel nisl id orci bibendum convallis at a purus. Quisque ut tortor sagittis, varius felis quis, imperdiet elit.'},
+        {title: 'forum3',user: 'angry1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper tincidunt nulla, ut hendrerit ex auctor eget. Praesent a molestie nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel nisl id orci bibendum convallis at a purus. Quisque ut tortor sagittis, varius felis quis, imperdiet elit.'}],
+        users: [
+            'angryperson1', 'angryperson2', 'angryperson2', 'angryperson2', 'angryperson2'
+        ]
+    }
     return (
         <div>
             dashboard
