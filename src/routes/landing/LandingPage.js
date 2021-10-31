6e9760303b43466f618eb8a58bd9b68c56ce0058
Robin <robinkhiv@gmail.com>
2021-10-31 01:31:08 -0700

    added routes in landing page

--- src/routes/landing/LandingPage.js
+++ src/routes/landing/LandingPage.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
-    <div class="landing">
-      <h1 class="welcome">Welcome Angry Nerds!</h1>
-      <h2>Become an angry nerd today.</h2>
-      <div class="btns">
-        <button type="button" class="btn btn-info" value="signup">Sign up</button>
+    <section class="landing">
+      <div className="container-xl">
+        <h1 class="welcome">Welcome Angry Nerds!</h1>
+        <h2>Become an angry nerd today.</h2>
+        <div class="btns">
+          <Link to="/register" className="btn btn-info">Sign Up</Link> 
+        </div>
+        <h2>Already an angry nerd? Log in.</h2>
+        <div class="btns">
+          <Link to="/login" className="btn btn-info">Login</Link> 
+        </div>
       </div>
-      <h2>Already an angry nerd? Log in.</h2>
-      <div class="btns">
-        <button type="button" class="btn btn-info" value="login">Log in</button>
-      </div>
-    </div>
+    </section>
   );
 }
