97c2816ad3117bff59fba28d9952c9238adca43c
Uyen-nt-Le <uyenle8897@csu.fullerton.edu>
2021-10-30 17:23:40 -0700

    added bootstrap

--- src/routes/landing/LandingPage.js
+++ src/routes/landing/LandingPage.js
@@ -4,14 +4,14 @@ import "./LandingPage.css";
 export default function LandingPage() {
   return (
     <div class="landing">
-      <h1>Welcome Angry Nerds!</h1>
+      <h1 class="welcome">Welcome Angry Nerds!</h1>
       <h2>Become an angry nerd today.</h2>
-      <div class="btn">
-        <button type="button">Sign up</button>
+      <div class="btns">
+        <button type="button" class="btn btn-info" value="signup">Sign up</button>
       </div>
       <h2>Already an angry nerd? Log in.</h2>
-      <div class="btn">
-        <button type="button">Log in</button>
+      <div class="btns">
+        <button type="button" class="btn btn-info" value="login">Log in</button>
       </div>
     </div>
   );
