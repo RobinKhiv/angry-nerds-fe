6eaaba2cf8bfdb1e09463647da93c53c4aeb386b
tukevin2 <tukevin@csu.fullerton.edu>
2021-10-31 19:13:27 -0700

    added form for forumform

--- src/components/forms/forum/forumform.js
+++ src/components/forms/forum/forumform.js
@@ -13,11 +13,20 @@ export default function forumform() {
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">New Forum Post</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                             ...
+                            <form>
+                                <div class="form-group">
+                                    <label for="title" class="col-form-label"></label>
+                                    <input type="text" class="form-control" placeholder="Title" id="title"></input>
+                                </div>
+                                <div class="form-group">
+                                    <label for="content" class="col-form-label"></label>
+                                    <textarea class="form-control" placeholder="Content" id="content"></textarea>
+                                </div>
+                            </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
