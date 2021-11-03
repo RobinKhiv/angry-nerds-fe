3736ade4ffde854c4339c833b816684f7a7db5b9
Uyen-nt-Le <uyenle8897@csu.fullerton.edu>
2021-11-02 20:37:18 -0700

    css landing page

--- src/routes/landing/LandingPage.js
+++ src/routes/landing/LandingPage.js
@@ -5,33 +5,68 @@ import "./LandingPage.css";
 export default function LandingPage() {
   return (
     <section className="landing">
-      <img className="angrynerds" src="static/angrynerds.jpg" alt="Angry Nerds"/>
+      <div class="img-top">
+        <img
+          className="angrynerds"
+          src="static/angrynerds.jpg"
+          alt="Angry Nerds"
+        />
+      </div>
+
       <div className="container-x1">
+        <div class="sign-top">
         <h3 class="text">Angry Nerds</h3>
         <h3 class="text">The place to release your anger.</h3>
 
-        <div className="btns"> 
-        <Link to="/register" className="btn btn-info">Sign Up</Link> 
+        <div className="btns">
+          <Link to="/register" className="btn btn-info">
+            Sign Up
+          </Link>
+        </div>
         </div>
         <h1 class="welcome">Welcome Angry Nerds!</h1>
         <h3 className="text">Learn More</h3>
-
-        <img className="getnoticed" src="static/getnoticed.png "alt="Get Noticed"/>
-        <img className="expressyourself" src="static/expressyourself.png" alt="Express Yourself"/>
-        <img className="agreewithothers" src="static/agreewithothers.png" alt="Agree With Others"/>
-
+        <div class="icon-container">
+          <div class="icon-img1">
+          <img
+            className="getnoticed"
+            src="static/getnoticed.png "
+            alt="Get Noticed"
+          />
+          </div>
+          <div class="icon-img2">
+          <img
+            className="expressyourself"
+            src="static/expressyourself.png"
+            alt="Express Yourself"
+          />
+          </div>
+          <div class="icon-img3">
+          <img
+            className="agreewithothers"
+            src="static/agreewithothers.png"
+            alt="Agree With Others"
+          />
+          </div>
+        </div>
+        <div class="icon-title">
         <h3 className="noticed">GET NOTICED</h3>
         <h3 className="express">EXPRESS YOURSELF</h3>
         <h3 className="agree">AGREE WITH OTHERS</h3>
+        </div>
 
         <h2>Become an angry nerd today.</h2>
         <div className="btns">
-          <Link to="/register" className="btn btn-info">Sign Up</Link> 
+          <Link to="/register" className="btn btn-info">
+            Sign Up
+          </Link>
         </div>
-        
+
         <h2>Already an angry nerd? Log in.</h2>
         <div className="btns">
-          <Link to="/login" className="btn btn-info">Login</Link> 
+          <Link to="/login" className="btn btn-info">
+            Login
+          </Link>
         </div>
       </div>
     </section>
